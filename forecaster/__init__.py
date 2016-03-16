36c86709964b51bafaee5cde5e10aa4d0b74e9ca
Scott Schmidt <sjschmidt44@gmail.com>
2016-03-16 16:08:50 -0700

    Update config in init.py

--- forecaster/__init__.py
+++ forecaster/__init__.py
@@ -5,10 +5,15 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
-    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('forecast', '/api_v1')
+    _add_static_views(config)
     config.scan()
     return config.make_wsgi_app()
+
+
+def _add_static_views(config):
+    config.add_static_view(name='fonts', path='forecaster:static/fonts')
+    config.add_static_view(name='css', path='forecaster:static/css')
+    config.add_static_view(name='js', path='forecaster:static/js')
